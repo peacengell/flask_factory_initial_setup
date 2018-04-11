ae543bcec067781ed5318ef54de0beb797978862
manoj.maracheea <manoj.maracheea@redboxdigital.com>
2018-04-11 21:54:31 +0400

    register the first view

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ from flask import Flask
 def create_app():
     app =Flask(__name__)
     app.config.from_pyfile('settings.py')
+
+    from home.views import home_app
+    app.register_blueprint(home_app)
+
     return app # return the whole flask app.
+
